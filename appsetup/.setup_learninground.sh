be64f739054027ac0b63a6ed5311e7b33fa6cbab
Bryce Caine <brycecaine@gmail.com>
2015-06-16 09:40:18 +0000

    Added env vars setup

--- appsetup/.setup_learninground.sh
+++ appsetup/.setup_learninground.sh
@@ -1,4 +1,6 @@
 export DJANGO_SETTINGS_MODULE=learninground.settings
+source ~/envvars/learninground.sh
 cd ~/workspace/learninground
 source ~/.venvs/learninground/bin/activate
 cd ~/workspace/learninground/wsgi/learninground
+source ~/envvars/learninground.sh
